220c87852ab56599feecf3fa7ab3508731a2c8c3
Faruk ERAT <farukerat1327@gmail.com>
2024-03-30 23:18:14 +0300

    local connection string removed

--- Authentication/src/Persistence/Configuration.cs
+++ Authentication/src/Persistence/Configuration.cs
@@ -13,14 +13,7 @@ public sealed class ConnectionStrings
     {
         string postgresEnvironmentKey = "POSTGRES_CONNECTION_STRING";
         string? postgres = Environment.GetEnvironmentVariable(postgresEnvironmentKey);
-        if (string.IsNullOrEmpty(postgres))
-        {
-            Postgres = "Host=localhost;Database=NoteTrees;Username=postgres;Password=root;Pooling=true;";
-            Environment.SetEnvironmentVariable(postgresEnvironmentKey, Postgres);
-        }
-        else
-        {
-            Postgres = postgres;
-        }
+        ArgumentNullException.ThrowIfNull(postgres, "Postgres connection string is required");
+        Postgres = postgres;
     }
 }
