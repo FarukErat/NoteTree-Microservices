551966b8d361c96ba213d7cfa5114da93b0ef81c
Faruk ERAT <farukerat1327@gmail.com>
2024-04-26 02:25:13 +0300

    removed done todos

--- Authentication/src/Application/UseCases/Login/Response.cs
+++ Authentication/src/Application/UseCases/Login/Response.cs
@@ -1,6 +1,5 @@
 namespace Application.UseCases.Login;
 
-// TODO: consider removing UserId from LoginResponse
 public sealed record class LoginResponse(
     Guid UserId,
     string Token);
--- Authentication/src/Infrastructure/Common/RsaKeyPair.cs
+++ Authentication/src/Infrastructure/Common/RsaKeyPair.cs
@@ -2,7 +2,6 @@ using Application.Interfaces.Infrastructure;
 
 namespace Infrastructure.Common;
 
-// TODO: consider sending PublicKey as byte[] instead of string in gRPC services
 public sealed class RsaKeyPair(byte[] privateKey, byte[] publicKey) : IRsaKeyPair
 {
     public byte[] PrivateKey { get; } = privateKey;
