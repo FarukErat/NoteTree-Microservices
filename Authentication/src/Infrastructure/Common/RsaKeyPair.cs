86234f0eea44a7e3c6015c4a63f1f81e2a0c6cb0
Faruk ERAT <farukerat1327@gmail.com>
2024-04-26 00:16:05 +0300

    added todo

--- Authentication/src/Infrastructure/Common/RsaKeyPair.cs
+++ Authentication/src/Infrastructure/Common/RsaKeyPair.cs
@@ -2,6 +2,7 @@ using Application.Interfaces.Infrastructure;
 
 namespace Infrastructure.Common;
 
+// TODO: consider sending PublicKey as byte[] instead of string in gRPC services
 public sealed class RsaKeyPair(byte[] privateKey, string publicKey) : IRsaKeyPair
 {
     public byte[] PrivateKey { get; } = privateKey;
