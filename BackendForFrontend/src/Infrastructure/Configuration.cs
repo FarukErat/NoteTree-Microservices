cdfbe2695d4d0b01596d185818cd13b386ed193b
Faruk ERAT <farukerat1327@gmail.com>
2024-05-27 01:15:44 +0300

    docker compose and config simplified

--- BackendForFrontend/src/Infrastructure/Configuration.cs
+++ BackendForFrontend/src/Infrastructure/Configuration.cs
@@ -15,16 +15,11 @@ public sealed class ConnectionStrings
 
     public ConnectionStrings()
     {
-        string? redis = Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING");
-        ArgumentNullException.ThrowIfNull(redis, "Redis connection string is required");
-        Redis = redis;
-
-        string? authenticationServer = Environment.GetEnvironmentVariable("AUTHENTICATION_SERVICE_URL");
-        ArgumentNullException.ThrowIfNull(authenticationServer, "Authentication server connection string is required");
-        AuthenticationServiceUrl = authenticationServer;
-
-        string? noteTreeServer = Environment.GetEnvironmentVariable("NOTE_TREE_SERVICE_URL");
-        ArgumentNullException.ThrowIfNull(noteTreeServer, "Note tree server connection string is required");
-        NoteTreeServiceUrl = noteTreeServer;
+        Redis = Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING")
+            ?? throw new ArgumentNullException("REDIS_CONNECTION_STRING is not set in the environment variables.");
+        AuthenticationServiceUrl = Environment.GetEnvironmentVariable("AUTHENTICATION_SERVICE_URL")
+            ?? throw new ArgumentNullException("AUTHENTICATION_SERVICE_URL is not set in the environment variables.");
+        NoteTreeServiceUrl = Environment.GetEnvironmentVariable("NOTE_TREE_SERVICE_URL")
+            ?? throw new ArgumentNullException("NOTE_TREE_SERVICE_URL is not set in the environment variables.");
     }
 }
