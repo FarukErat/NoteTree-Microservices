bb8503a25348a72f6f9829618b1155450e58ef0c
Faruk ERAT <farukerat1327@gmail.com>
2024-05-31 01:39:26 +0300

    soft delete notes taken

--- Authentication/src/Domain/Entities/BaseEntity.cs
+++ Authentication/src/Domain/Entities/BaseEntity.cs
@@ -6,4 +6,29 @@ public class BaseEntity
     // public Guid TenantId { get; set; }
     // public DateTime CreatedAt { get; set; }
     // public DateTime UpdatedAt { get; set; }
+    // public DateTime? DeletedAt { get; set; }
 }
+
+// public class SoftDeleteInterceptor : SaveChangesInterceptor
+// {
+//     public override InterceptionResult<int> SavingChanges(
+//         DbContextEventData eventData,
+//         InterceptionResult<int> result)
+//     {
+//         foreach (var entry in eventData.Context.ChangeTracker.Entries())
+//         {
+//             if (entry.Entity is BaseEntity entity)
+//             {
+//                 switch (entry.State)
+//                 {
+//                     case EntityState.Deleted:
+//                         entry.State = EntityState.Modified;
+//                         entity.DeletedAt = DateTime.UtcNow;
+//                         break;
+//                 }
+//             }
+//         }
+
+//         return base.SavingChanges(eventData, result);
+//     }
+// }
