5cb844edc09f9059a47f60d187e6144470056cde
Faruk ERAT <farukerat1327@gmail.com>
2024-05-31 22:21:42 +0300

    todo removed

--- Authentication/src/Domain/Entities/User.cs
+++ Authentication/src/Domain/Entities/User.cs
@@ -10,7 +10,6 @@ public sealed class User : BaseEntity
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public uint Roles { get; set; }
-    // TODO: consider embedding the password hash algorithm and its parameters in the password hash
     public string PasswordHash { get; set; } = string.Empty;
     public PasswordHashAlgorithm PasswordHashAlgorithm { get; set; }
 }
