0b38f8842ccf2063ab27ee3136ee7a636f666fca
Faruk ERAT <farukerat1327@gmail.com>
2024-06-01 15:03:28 +0300

    done a todo

--- BackendForFrontend/src/Infrastructure/Common/CacheService.cs
+++ BackendForFrontend/src/Infrastructure/Common/CacheService.cs
@@ -18,12 +18,8 @@ public sealed class CacheService
     public async Task<Guid> SaveSessionAsync(Session session)
     {
         session.Id = Guid.NewGuid();
-        string rawId = await _sessions
-            .InsertAsync(session, session.ExpireAt - DateTime.UtcNow);
-        string id = rawId.Split(':')[1];
-        // TODO: is this necessary after assigning session.Id with Guid.NewGuid()?
-        Guid result = Guid.Parse(id);
-        return result;
+        await _sessions.InsertAsync(session, session.ExpireAt - DateTime.UtcNow);
+        return session.Id;
     }
 
     public async Task<Session?> GetSessionByIdAsync(Guid sessionId)
