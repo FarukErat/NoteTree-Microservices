42f5b174d4de743e46aa6b95ae738c9886178e7d
Faruk <farukerat1327@gmail.com>
2024-08-12 21:30:59 +0300

    added a simple script to build and run all docker compose files

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-set -e
+docker compose -f ./MessageBroker/docker-compose.yml up --build -d
+docker compose -f ./Log/docker-compose.yml up --build -d
 
 docker compose -f ./Authentication/docker-compose.yml up --build -d
-docker compose -f ./BackendForFrontend/docker-compose.yml up --build -d
-docker compose -f ./Log/docker-compose.yml up --build -d
-docker compose -f ./MessageBroker/docker-compose.yml up --build -d
 docker compose -f ./NoteTree/docker-compose.yml up --build -d
+docker compose -f ./BackendForFrontend/docker-compose.yml up --build -d
