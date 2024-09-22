c773e4783d36a707ae161bc25d5b78c758e47f8a
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2024-09-23 00:13:01 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 (cd ./MessageBroker && docker compose up --build -d)
 (cd ./Log && docker compose up --build -d)
 
-(cd ./Authentication && cat .env.example > .env && docker compose up --build -d)
-(cd ./NoteTree && cat .env.example > .env && docker compose up --build -d)
-(cd ./BackendForFrontend && cat .env.example > .env && docker compose up --build -d)
+(cd ./Authentication && cp .env.example .env && docker compose up --build -d)
+(cd ./NoteTree && cp .env.example .env && docker compose up --build -d)
+(cd ./BackendForFrontend && cp .env.example .env && docker compose up --build -d)
