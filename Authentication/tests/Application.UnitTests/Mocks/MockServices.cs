84f8b3079628fed5fd8c0fd7ce0cde716288b5f3
Faruk <farukerat1327@gmail.com>
2024-10-06 23:51:59 +0300

    remove redundant codes

--- Authentication/tests/Application.UnitTests/Mocks/MockServices.cs
+++ Authentication/tests/Application.UnitTests/Mocks/MockServices.cs
@@ -11,32 +11,6 @@ namespace Mocks;
 
 public static class MockServices
 {
-    public static readonly ISender Sender;
-
-    static MockServices()
-    {
-        WebApplicationBuilder builder = WebApplication.CreateBuilder();
-
-        // register services
-        builder.Services.AddSingleton(Substitute.For<IUserReadRepository>());
-        builder.Services.AddSingleton(Substitute.For<IUserWriteRepository>());
-        builder.Services.AddSingleton(Substitute.For<IPasswordHasherFactory>());
-        builder.Services.AddSingleton(Substitute.For<IMessageBroker>());
-        builder.Services.AddSingleton(Substitute.For<IJwtGenerator>());
-
-        // register pipeline
-        builder.Services.AddValidatorsFromAssemblyContaining(typeof(DependencyInjection));
-
-        // register MediatR
-        builder.Services.AddMediatR(cfg =>
-        {
-            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
-            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
-        });
-
-        Sender = builder.Services.BuildServiceProvider().GetRequiredService<ISender>();
-    }
-
     public static async Task<TResponse> Validate<TRequest, TResponse>(
         IRequest<TResponse> request,
         IEnumerable<IValidator<TRequest>> validators)
