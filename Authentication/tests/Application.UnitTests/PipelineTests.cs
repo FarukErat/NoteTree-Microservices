eda6cee2972ed8fe2c33e97eaae2581f2eb7f49f
Faruk <farukerat1327@gmail.com>
2024-10-07 01:08:43 +0300

    add log tests

--- Authentication/tests/Application.UnitTests/PipelineTests.cs
+++ Authentication/tests/Application.UnitTests/PipelineTests.cs
@@ -1,6 +1,9 @@
+using Application.Behaviors;
 using Application.UseCases.Register;
 using ErrorOr;
 using FluentAssertions;
+using MediatR;
+using NSubstitute;
 using static Mocks.MockServices;
 
 namespace Tests;
@@ -47,5 +50,33 @@ public class PipelineTests
 
         // Assert
         response.IsError.Should().BeFalse("request is valid");
+    }
+
+    [Fact]
+    public async void LoggingBehavior_WhenRequestHandled_ShouldLog()
+    {
+        // Arrange
+        RegisterRequest request = new(
+            Username: "username",
+            Password: "password",
+            Email: "email@example.com",
+            FirstName: "first name",
+            LastName: "last name"
+        );
+
+        ILogger<LoggingBehavior<RegisterRequest, ErrorOr<RegisterResponse>>> logger
+            = Substitute.For<ILogger<LoggingBehavior<RegisterRequest, ErrorOr<RegisterResponse>>>>();
+
+        LoggingBehavior<RegisterRequest, ErrorOr<RegisterResponse>> loggingBehavior = new(logger);
+
+        // Act
+        await loggingBehavior.Handle(
+            request,
+            () => Task.FromResult(new ErrorOr<RegisterResponse>()),
+            CancellationToken.None);
+
+        // Assert
+        logger.Received().LogInformation("Handling {@Request}", request);
+        logger.Received().LogInformation("Handled {@Request} in {ElapsedMilliseconds}ms", request, Arg.Any<long>());
     }
 }
