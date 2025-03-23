91475c68dbd35a48fc2272315bc50a8cbbed18e8
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2025-03-23 15:48:18 +0300

    Create scaffolder.sh

---
+++ scaffolder.sh
@@ -0,0 +1,31 @@
+set -eu
+
+PROJECT_NAME=$1
+
+( mkdir ${PROJECT_NAME} && cd ${PROJECT_NAME} # Project Directory
+
+    ( mkdir src && cd src # Source Directory
+
+        # Source Solution
+        dotnet new sln -n "${PROJECT_NAME}"
+
+        # Project Layers
+        dotnet new classlib -n "${PROJECT_NAME}.Domain"
+        dotnet new classlib -n "${PROJECT_NAME}.Application"
+        dotnet new classlib -n "${PROJECT_NAME}.Infrastructure"
+        dotnet new classlib -n "${PROJECT_NAME}.Presentation"
+
+        # Add Projects to the Solution
+        dotnet sln add "${PROJECT_NAME}.Domain/${PROJECT_NAME}.Domain.csproj"
+        dotnet sln add "${PROJECT_NAME}.Application/${PROJECT_NAME}.Application.csproj"
+        dotnet sln add "${PROJECT_NAME}.Infrastructure/${PROJECT_NAME}.Infrastructure.csproj"
+        dotnet sln add "${PROJECT_NAME}.Presentation/${PROJECT_NAME}.Presentation.csproj"
+
+        # Add Project References
+        dotnet add "${PROJECT_NAME}.Application/${PROJECT_NAME}.Application.csproj" reference "${PROJECT_NAME}.Domain/${PROJECT_NAME}.Domain.csproj"
+        dotnet add "${PROJECT_NAME}.Infrastructure/${PROJECT_NAME}.Infrastructure.csproj" reference "${PROJECT_NAME}.Application/${PROJECT_NAME}.Application.csproj"
+        dotnet add "${PROJECT_NAME}.Presentation/${PROJECT_NAME}.Presentation.csproj" reference "${PROJECT_NAME}.Application/${PROJECT_NAME}.Application.csproj"
+        dotnet add "${PROJECT_NAME}.Presentation/${PROJECT_NAME}.Presentation.csproj" reference "${PROJECT_NAME}.Infrastructure/${PROJECT_NAME}.Infrastructure.csproj"
+    )
+
+)
